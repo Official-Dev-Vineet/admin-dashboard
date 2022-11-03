ccdfe32a81f9286b76b9d02b581cec1fe9fbde80
Official-Dev-Vineet <viratsinghkaharwar8923@gmail.com>
2022-11-03 20:21:16 +0530

    nav added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { BrowserRouter, Link, Routes, Route, NavLink } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+      <nav style={{color:'#000'}}>
+        <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>{" "}
+        <li>
+          <NavLink to="/about">about</NavLink>
+        </li>{" "}
+        <li>
+          <NavLink to="/contact">contact</NavLink>
+        </li>
+        </ul>
+      </nav>
+        <Routes>
+          <Route path="/" element={<div> home </div>} />
+          <Route path="/about" element={<div> about </div>} />
+          <Route path="/contact" element={<div> contact </div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
